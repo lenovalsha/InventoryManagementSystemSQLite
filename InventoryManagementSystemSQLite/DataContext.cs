bee724f09db4ef2125d62a10b3927aba53acba28
Leila Noval <109560883+lenovalsha@users.noreply.github.com>
2025-09-04 15:00:27 -0600

    Add project files.

---
+++ InventoryManagementSystemSQLite/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystemSQLite
+{
+    public class DataContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = InventoryManagementSystem.db");
+        }
+
+        //we need to define the tables in our database
+        //Create the tables
+        public DbSet<Models.Category> Category { get; set; }
+        public DbSet<Models.ProductType> ProductType { get; set; }
+        public DbSet<Models.Product> Product { get; set; }
+        public DbSet<Models.Supplier> Supplier { get; set; }
+
+
+    }
+}
---
+++ InventoryManagementSystemSQLite/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//ADDED THIS
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace InventoryManagementSystemSQLite
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            //ADDED THIS
+            //this ensures that the database file is created if it doesnt exist
+            DatabaseFacade facade = new DatabaseFacade(new DataContext());
+            facade.EnsureCreated();
+        }
+      
+    }
+}
